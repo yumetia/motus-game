98cb41f9990a0bf3a312f0e1f8e8e7f78bd789cc
yumetia <yumetia000@gmail.com>
2026-03-26 21:07:49 +0100

    chore: routing ok

--- index.php
+++ index.php
@@ -1,36 +1,22 @@
 <?php
 session_start();
+$routes = [
+    "/login" => "server/view/login.php",
+    "/register" => "server/view/register.php",
+    "/game" => "server/view/game.php",
+];
 
-if (!isset($_SESSION["user"])){
-    header("Location: /login");
-    exit();
-}
+$url = $_SERVER['REQUEST_URI'] ?? "/login";
 
-// since the user is logged, we get the current url so we can redirect to it but:
+// since the user is logged from there, we get the current url so we can redirect to it but:
 // if we know this url==> OK redirect
 // if we dont know this url==> 404
 
-$url = $_SERVER['REQUEST_URI'];
+if (array_key_exists($url, $routes)){
+    $page = $routes[$url];
+    require_once $page;
+} else {
+    require_once 'html/404.html';
+}
 
-
-$routes = [
-    "login" => "server/view/login.php",
-    "register" => "server/view/register.php",
-    "game" => "server/view/game.php",
-]
-
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Motus Game</title>
-</head>
-<body>
-    
-</body>
-</html>
 
